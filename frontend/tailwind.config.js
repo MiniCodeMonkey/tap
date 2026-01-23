b2419478e9c8bafae0a1172f3984949b0af24c8b
Mathias Hansen <me@codemonkey.io>
2026-01-24 00:36:19 +0100

    feat: [US-101] - Install and configure TailwindCSS in frontend build

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './presenter.html',
+    './src/**/*.{svelte,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
