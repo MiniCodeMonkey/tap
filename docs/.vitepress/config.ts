32d9b047b47ac18b8f9c8fc8d5cd684dfac5c929
Mathias Hansen <me@codemonkey.io>
2026-01-23 21:01:25 +0100

    feat: [US-001] - Initialize VitePress project

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'Tap',
+  description: 'Presentations for developers',
+
+  srcExclude: ['**/ralph/**'],
+
+  themeConfig: {
+    // Will be configured in future stories
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+---
+layout: home
+---
+
+# Tap
+
+Presentations for developers.
